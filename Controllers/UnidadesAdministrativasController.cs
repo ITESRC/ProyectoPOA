7cb43c40e6bb1743ea267160ce6d4bbd3ce37255
Jesus Gonzalez Rojas <jes_uz@hotmail.com>
2020-02-07 01:45:16 -0600

    ViewModelUnidadAdministrativa, Funcion Agregar y Consultas de Superior

--- Controllers/UnidadesAdministrativasController.cs
+++ Controllers/UnidadesAdministrativasController.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProyectoPOA.Models.ViewModels;
 using ProyectoPOA.Repositories;
 
 namespace ProyectoPOA.Controllers
 {
     public class UnidadesAdministrativasController : Controller
     {
+        UnidadAdministrativaRepository repository;
         public IActionResult Index()
         {
-            UnidadAdministrativaRepository repository = new UnidadAdministrativaRepository();
+             repository = new UnidadAdministrativaRepository();
             return View(repository.GetAll());
         }
+        //Mostrar el formulario de Agregar
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        //Agregar todos los campos del formulario
+        public IActionResult Agregar(UnidadAdministrativasViewModel unidad)
+        {
+            repository = new UnidadAdministrativaRepository();
+            if (ModelState.IsValid)
+            {
+                repository.Context.Add(unidad);
+                repository.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(unidad);
+            }
+        }
+
     }
 }
---
+++ Models/ViewModels/UnidadAdministrativasViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoPOA.Models.ViewModels
+{
+    public class UnidadAdministrativasViewModel
+    {
+        public int Id { get; set; }
+        public int Clave { get; set; }
+        public string Encargado { get; set; }
+        public string Superior { get; set; }
+    }
+}
