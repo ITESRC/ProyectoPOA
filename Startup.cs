32f346f49c2c99ce88d49882cb1ae085e381877f
Marco Antonio Martinez <marco_scarface.77@hotmail.com>
2020-02-07 17:24:00 -0600

    Perfil creado de web deploy

--- Startup.cs
+++ Startup.cs
@@ -13,7 +13,7 @@ namespace ProyectoPOA
     {
         public IHostingEnvironment Environment { get; set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
