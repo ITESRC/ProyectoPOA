718bed9f8de4a1ec90ac0fae64261d07dca7bf6a
Jesus Gonzalez Rojas <jes_uz@hotmail.com>
2020-03-28 05:47:10 -0600

    Vista,Repositorio y Controller de objetivo,Correcciones en codigo de submit

---
+++ Repositories/ObjetivoRepository.cs
@@ -0,0 +1,59 @@
+﻿using ProyectoPOA.Models;
+using ProyectoPOA.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoPOA.Repositories
+{
+    public class ObjetivoRepository : Repository<Objetivo>
+    {
+        public IEnumerable<Objetivo> GetObjetivo()
+        {
+            var Vigente = Context.Objetivo.Where(x => x.Eliminado == true);
+            return Vigente;
+        }
+        public IEnumerable<ObjetivosEstrategiasViewModel> GetEstrategias()
+        {
+            ObjetivoRepository repos = new ObjetivoRepository();
+            return repos.GetAll().OrderBy(x => x.Nombre).Where(x => x.Eliminado == false).Select(x => new ObjetivosEstrategiasViewModel { Id = x.Id, Nombre = x.Nombre });
+        }
+        public List<Estrategia> GetEstrategiasByObjetivo(int IdObjetivo)
+        {
+            var estra = Context.Estrategia.Where(x => x.IdObjetivo == IdObjetivo);
+
+            return estra.ToList();
+
+
+        }
+
+        //public string GetEstrategiasByObjetivo(int IdObjetivo)
+        //{
+        //    var estra = Context.Estrategia.Where(x => x.IdObjetivo == IdObjetivo);
+        //    var script = "<select>< option selected = 'selected' value = '' > --Estrategias-- </ option ></ select >";
+        //    while (estra != null)
+        //    {
+        //        script = "<select asp-for='@Model.Id'  asp-items='@(new SelectList(" + estra.ToList() + ", 'Nombre'))'>< option selected = 'selected' value = '' > --Estrategias-- </ option ></ select > ";
+        //        return script;
+        //    }
+        //    return script;
+
+
+        //}
+        public List<string> Validar(Objetivo VO)
+        {
+            List<string> errores = new List<string>();
+            var repetido = Context.Objetivo.Any(x => x.Nombre == VO.Nombre && x.Eliminado == false);
+
+            if (repetido)
+            {
+                errores.Append("No puede haber más de un objetivo igual");
+            }
+
+            return errores;
+        }
+
+
+    }
+}
