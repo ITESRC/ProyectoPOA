21e5fd6d02db7a1a293b2e10cb5a1b661f7a3abe
Marco Antonio Escobar Martinez <161G0209@rcarbonifera.tecnm.mx>
2020-05-13 17:39:51 -0500

    permitir estrategias

---
+++ Controllers/PermitirEstrategiasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoPOA.Controllers
+{
+    public class PermitirEstrategiasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Repositories/PermitirEstrategiaRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProyectoPOA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoPOA.Repositories
+{
+    public class PermitirEstrategiaRepository:Repository<Uaestrategia>
+    {
+        
+    }
+}
